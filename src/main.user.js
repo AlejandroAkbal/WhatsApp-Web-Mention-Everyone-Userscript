cc59bdd696f1e69562c5a316873812d29e2c2396
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-23 13:56:40 +0100

    chore: rename repo

--- src/main.user.js
+++ src/main.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name            WhatsApp Web Tag Everyone
+// @name            WhatsApp Web Mention Everyone
 // @namespace       AlejandroAkbal
 // @version         0.1
 // @description     Automatically tag everyone in a group chat on WhatsApp Web
 // @author          Alejandro Akbal
 // @license         AGPL-3.0
 // @icon            https://www.google.com/s2/favicons?sz=64&domain=whatsapp.com
-// @homepage        https://github.com/AlejandroAkbal/WhatsApp-Web-Tag-Everyone-Userscript
-// @downloadURL     https://raw.githubusercontent.com/AlejandroAkbal/WhatsApp-Web-Tag-Everyone-Userscript/main/src/main.user.js
-// @updateURL       https://raw.githubusercontent.com/AlejandroAkbal/WhatsApp-Web-Tag-Everyone-Userscript/main/src/main.user.js
+// @homepage        https://github.com/AlejandroAkbal/WhatsApp-Web-Mention-Everyone-Userscript
+// @downloadURL     https://raw.githubusercontent.com/AlejandroAkbal/WhatsApp-Web-Mention-Everyone-Userscript/main/src/main.user.js
+// @updateURL       https://raw.githubusercontent.com/AlejandroAkbal/WhatsApp-Web-Mention-Everyone-Userscript/main/src/main.user.js
 // @match           https://web.whatsapp.com/*
 // @grant           none
 // @run-at          document-idle
@@ -80,7 +80,7 @@ async function waitForElement(selector, options = {}) {
 ;(async function () {
     'use strict'
 
-    console.info('WhatsApp Web Tag Everyone loaded.')
+    console.info('WhatsApp Web Mention Everyone loaded.')
 
     let buffer = "";
 
