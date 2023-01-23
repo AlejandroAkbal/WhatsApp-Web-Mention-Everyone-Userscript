c8ef4b6a214dfff50031b7d8f26495c017addf03
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-23 13:24:59 +0100

    feat: create main logic

--- src/main.user.js
+++ src/main.user.js
@@ -14,7 +14,121 @@
 // @run-at          document-idle
 // ==/UserScript==
 
-;(function () {
+
+/** @param {number} ms
+ * @returns {Promise<void>}
+ */
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+/**
+ * Wait for an element matching the given selector to appear in the DOM
+ * @param {string} selector - The CSS selector to match
+ * @param {Object} [options={}] - Additional options
+ * @param {number} [options.timeout=10000] - The number of milliseconds to wait before timing out
+ * @param {boolean} [options.subtree=true] - Whether to observe the entire subtree or just the target node
+ * @param {boolean} [options.childList=true] - Whether to observe added and removed nodes
+ * @returns {Promise<Element>} - A promise that resolves with the matched element
+ */
+async function waitForElement(selector, options = {}) {
+    const {
+        timeout = 10000,
+        subtree = true,
+        childList = true
+    } = options;
+
+    return new Promise((resolve, reject) => {
+        let element;
+        let timeoutId;
+
+        const observer = new MutationObserver((mutations) => {
+            for (const mutation of mutations) {
+                for (const node of mutation.addedNodes) {
+                    if (node.matches && node.matches(selector)) {
+                        element = node;
+
+                        observer.disconnect();
+
+                        clearTimeout(timeoutId);
+
+                        resolve(node);
+                        return;
+                    }
+                }
+            }
+        });
+
+        observer.observe(document.documentElement, {subtree, childList});
+
+        timeoutId = setTimeout(() => {
+
+            observer.disconnect();
+
+            if (element) {
+                resolve(element);
+
+            } else {
+                reject(new Error(`Element not found: ${selector}`));
+            }
+
+        }, timeout);
+    });
+}
+
+
+;(async function () {
     'use strict'
 
+    console.log('WhatsApp Web Tag Everyone loaded')
+
+    async function tagEveryone() {
+        const chatSubtitle = document.querySelector("[data-testid='chat-subtitle'] > span")
+
+        if (!chatSubtitle) {
+            throw new Error('No chat subtitle found, please open a group chat.')
+        }
+
+        let groupUsers = chatSubtitle.innerText.split(', ')
+
+        // Remove unnecessary text
+        groupUsers = groupUsers.filter(user => user !== 'You')
+
+        // Normalize user's names without accents or special characters
+        groupUsers = groupUsers.map(user => user.normalize('NFD').replace(/[\u0300-\u036f]/g, ''))
+
+        const chatInput = document.querySelector("[data-testid='conversation-compose-box-input'] > p")
+
+        if (!chatInput) {
+            throw new Error('No chat input found. Please type a letter in the chat input.')
+        }
+
+        console.debug('Chat input:', chatInput)
+
+        for (const user of groupUsers) {
+            chatInput.focus()
+
+            document.execCommand('insertText', false, `@${user}`)
+
+            // await waitForElement("[data-testid='contact-mention-list-item']")
+            await sleep(300)
+
+            // Send "tab" key to autocomplete the user
+            const keyboardEvent = new KeyboardEvent('keydown', {
+                key: 'Tab',
+                code: 'Tab',
+                keyCode: 9,
+                which: 9,
+                bubbles: true,
+                cancelable: true,
+                view: window,
+            })
+
+            chatInput.dispatchEvent(keyboardEvent)
+
+            document.execCommand('insertText', false, ' ')
+        }
+    }
+
+    await tagEveryone()
 })()
