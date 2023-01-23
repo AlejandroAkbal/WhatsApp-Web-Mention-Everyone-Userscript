82b780aa3187bb9910627c9522b0ab07b066c4a0
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-23 17:40:54 +0100

    feat: show errors in an alert

--- src/main.user.js
+++ src/main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            WhatsApp Web Mention Everyone
 // @namespace       AlejandroAkbal
-// @version         0.1
+// @version         0.1.1
 // @description     Automatically tag everyone in a group chat on WhatsApp Web
 // @author          Alejandro Akbal
 // @license         AGPL-3.0
@@ -14,12 +14,11 @@
 // @run-at          document-idle
 // ==/UserScript==
 
-
 /** @param {number} ms
  * @returns {Promise<void>}
  */
 function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 /**
@@ -32,117 +31,110 @@ function sleep(ms) {
  * @returns {Promise<Element>} - A promise that resolves with the matched element
  */
 async function waitForElement(selector, options = {}) {
-    const {
-        timeout = 10000,
-        subtree = true,
-        childList = true
-    } = options;
+  const { timeout = 10000, subtree = true, childList = true } = options
 
-    return new Promise((resolve, reject) => {
-        let element;
-        let timeoutId;
+  return new Promise((resolve, reject) => {
+    let element
+    let timeoutId
 
-        const observer = new MutationObserver((mutations) => {
-            for (const mutation of mutations) {
-                for (const node of mutation.addedNodes) {
-                    if (node.matches && node.matches(selector)) {
-                        element = node;
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        for (const node of mutation.addedNodes) {
+          if (node.matches && node.matches(selector)) {
+            element = node
 
-                        observer.disconnect();
+            observer.disconnect()
 
-                        clearTimeout(timeoutId);
+            clearTimeout(timeoutId)
 
-                        resolve(node);
-                        return;
-                    }
-                }
-            }
-        });
+            resolve(node)
+            return
+          }
+        }
+      }
+    })
 
-        observer.observe(document.documentElement, {subtree, childList});
+    observer.observe(document.documentElement, { subtree, childList })
 
-        timeoutId = setTimeout(() => {
+    timeoutId = setTimeout(() => {
+      observer.disconnect()
 
-            observer.disconnect();
-
-            if (element) {
-                resolve(element);
-
-            } else {
-                reject(new Error(`Element not found: ${selector}`));
-            }
-
-        }, timeout);
-    });
+      if (element) {
+        resolve(element)
+      } else {
+        reject(new Error(`Element not found: ${selector}`))
+      }
+    }, timeout)
+  })
 }
 
-
 ;(async function () {
-    'use strict'
+  'use strict'
 
-    console.info('WhatsApp Web Mention Everyone loaded.')
+  console.info('WhatsApp Web Mention Everyone loaded.')
 
-    let buffer = "";
+  let buffer = ''
 
-    document.addEventListener("keyup", event => {
-        buffer += event.key;
+  document.addEventListener('keyup', async (event) => {
+    buffer += event.key
 
-        // Keep the last 2 characters
-        buffer = buffer.slice(-2)
+    // Keep the last 2 characters
+    buffer = buffer.slice(-2)
 
-        console.log(buffer)
+    if (buffer === '@@') {
+      buffer = ''
 
-        if (buffer === "@@") {
-            buffer = "";
+      // TODO: Delete the last 2 written characters (the "@@")
 
-            // TODO: Delete the last 2 written characters (the "@@")
+      try {
+        await tagEveryone()
+      } catch (error) {
+        alert(error.message)
+      }
+    }
+  })
 
-            tagEveryone();
-        }
-    });
+  async function tagEveryone() {
+    const groupSubtitle = document.querySelector("[data-testid='chat-subtitle'] > span")
 
-    async function tagEveryone() {
-        const groupSubtitle = document.querySelector("[data-testid='chat-subtitle'] > span")
-
-        if (!groupSubtitle) {
-            throw new Error('No chat subtitle found, please open a group chat.')
-        }
-
-        let groupUsers = groupSubtitle.innerText.split(', ')
-
-        // Remove unnecessary text
-        groupUsers = groupUsers.filter(user => user !== 'You')
-
-        // Normalize user's names without accents or special characters
-        groupUsers = groupUsers.map(user => user.normalize('NFD').replace(/[\u0300-\u036f]/g, ''))
-
-        const chatInput = document.querySelector("[data-testid='conversation-compose-box-input'] > p")
-
-        if (!chatInput) {
-            throw new Error('No chat input found. Please type a letter in the chat input.')
-        }
-
-        for (const user of groupUsers) {
-            document.execCommand('insertText', false, `@${user}`)
-
-            // await waitForElement("[data-testid='contact-mention-list-item']")
-            await sleep(300)
-
-            // Send "tab" key to autocomplete the user
-            const keyboardEvent = new KeyboardEvent('keydown', {
-                key: 'Tab',
-                code: 'Tab',
-                keyCode: 9,
-                which: 9,
-                bubbles: true,
-                cancelable: true,
-                view: window,
-            })
-
-            chatInput.dispatchEvent(keyboardEvent)
-
-            document.execCommand('insertText', false, ' ')
-        }
+    if (!groupSubtitle) {
+      throw new Error('No chat subtitle found, please open a group chat.')
     }
 
+    let groupUsers = groupSubtitle.innerText.split(', ')
+
+    // Remove unnecessary text
+    groupUsers = groupUsers.filter((user) => user !== 'You')
+
+    // Normalize user's names without accents or special characters
+    groupUsers = groupUsers.map((user) => user.normalize('NFD').replace(/[\u0300-\u036f]/g, ''))
+
+    const chatInput = document.querySelector("[data-testid='conversation-compose-box-input'] > p")
+
+    if (!chatInput) {
+      throw new Error('No chat input found. Please type a letter in the chat input.')
+    }
+
+    for (const user of groupUsers) {
+      document.execCommand('insertText', false, `@${user}`)
+
+      // await waitForElement("[data-testid='contact-mention-list-item']")
+      await sleep(300)
+
+      // Send "tab" key to autocomplete the user
+      const keyboardEvent = new KeyboardEvent('keydown', {
+        key: 'Tab',
+        code: 'Tab',
+        keyCode: 9,
+        which: 9,
+        bubbles: true,
+        cancelable: true,
+        view: window
+      })
+
+      chatInput.dispatchEvent(keyboardEvent)
+
+      document.execCommand('insertText', false, ' ')
+    }
+  }
 })()
