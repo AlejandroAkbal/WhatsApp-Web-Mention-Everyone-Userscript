a4bd2b9092b3214434102b698ab94f653db31c44
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-23 10:40:21 +0100

    feat: add UserScript metadata

---
+++ src/main.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name            WhatsApp Web Tag Everyone
+// @namespace       AlejandroAkbal
+// @version         0.1
+// @description     Automatically tag everyone in a group chat on WhatsApp Web
+// @author          Alejandro Akbal
+// @license         AGPL-3.0
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=whatsapp.com
+// @homepage        https://github.com/AlejandroAkbal/WhatsApp-Web-Tag-Everyone-Userscript
+// @downloadURL     https://raw.githubusercontent.com/AlejandroAkbal/WhatsApp-Web-Tag-Everyone-Userscript/main/src/main.user.js
+// @updateURL       https://raw.githubusercontent.com/AlejandroAkbal/WhatsApp-Web-Tag-Everyone-Userscript/main/src/main.user.js
+// @match           https://web.whatsapp.com/*
+// @grant           none
+// @run-at          document-idle
+// ==/UserScript==
+
+;(function () {
+    'use strict'
+
+})()
